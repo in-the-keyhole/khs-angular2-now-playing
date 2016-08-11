29d645ce2b183974678d602e29dcee649399419d
Jaime Niswonger <jniswonger@keyholesoftware.com>
2016-08-11 11:15:56 -0500

    added close button to MovieComponent

--- app/movie.component.ts
+++ app/movie.component.ts
@@ -1,5 +1,5 @@
 import {Component} from "@angular/core";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {MovieService} from "./movie.service";
 import {Movie} from "./movie.model";
 @Component({
@@ -8,6 +8,7 @@ import {Movie} from "./movie.model";
         <div class="movie-detail" *ngIf="movie">
             <h2>{{movie.title}}</h2>
             <h3>{{movie.overview}}</h3>
+            <span class="close" (click)="close()"></span>
             <img src="http://image.tmdb.org/t/p/w1280{{movie.backdrop_path}}"/>
         </div>
     `,
@@ -17,11 +18,15 @@ export class MovieComponent {
 
     private movie:Movie;
 
-    constructor(route:ActivatedRoute, movieService:MovieService) {
+    constructor(private router:Router, route:ActivatedRoute, movieService:MovieService) {
         route.params.subscribe(params =>
             movieService.movie(params['id']).subscribe(movie =>
                 this.movie = movie
             )
         )
+    }
+
+    close() {
+        this.router.navigate(['/movies']);
     }
 }
